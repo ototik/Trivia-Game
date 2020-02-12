c29af61b44e221d0c44bcb3133be2aa389ff1aa1
Orsolya Totik <ototik@gmail.com>
2020-02-12 13:59:40 +0100

    commit 0212

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Router, Switch, Route } from "react-router-dom";
 
 import Game from "./Game/Game";
-import Gameselector from "./Gameselector/Gameselector";
 
 import Home from "./Home/Home";
 import history from "./history";
@@ -14,7 +13,6 @@ export default class Routes extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/Game" component={Game} />
-          <Route path="/Gameselector" component={Gameselector} />
         </Switch>
       </Router>
     );
