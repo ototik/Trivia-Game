fddd0991af123103e139c50a26720d74a5385465
SzMariann <mariann.szabo.90@gmail.com>
2020-02-26 13:08:25 +0100

    result display fixed and buttons are dynamic

--- src/components/Mquestions.js
+++ src/components/Mquestions.js
@@ -5,122 +5,135 @@ import history from "./../history";
 
 const datafromjson = data;
 class Mquestions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      question: "",
-      answers: [],
-      cor_answer: "",
-      current: 0,
-      max: 0,
-      score: 0,
-      apidata: ""
-    };
-    this.getQuestion = this.getQuestion.bind(this);
-    this.isRightAnswer = this.isRightAnswer.bind(this);
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
-
-  componentDidMount() {
-    axios
-      .get(datafromjson.url)
-      .then(response => response.data)
-      .then(data => {
-        this.setState({
-          apidata: data
-        });
-        this.getQuestion();
-      });
-  }
-
-  getQuestion() {
-    let { current, apidata, score, max, cor_answer } = this.state;
-    if (apidata.results.length === current) {
-      data.score = score;
-      data.max = max;
-      data.cor_answer = cor_answer;
-      console.log(data.score);
-      history.push("/Zsgameresult");
-    } else {
-      if (apidata.results[current].type === "multiple") {
-        this.setState({
-          question: apidata.results[current].question,
-          answers: [
-            [apidata.results[current].incorrect_answers[0]],
-            [apidata.results[current].incorrect_answers[1]],
-            [apidata.results[current].incorrect_answers[2]],
-            [apidata.results[current].correct_answer]
-          ].sort(() => Math.random() - 0.5),
-          cor_answer: apidata.results[current].correct_answer,
-          max: apidata.results.length,
-          current: current + 1
-        });
-      } else {
-        this.setState({
-          question: apidata.results[current].question,
-          answers: [["True"], ["False"]],
-          cor_answer: apidata.results[current].correct_answer,
-          max: apidata.results.length,
-          current: current + 1,
-          
-        });
-        console.log(this.state.apidata.results[current].type);
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: "",
+            answers: [],
+            cor_answer: "",
+            current: 0,
+            max: 0,
+            score: 0,
+            apidata: ""
+        };
+        this.getQuestion = this.getQuestion.bind(this);
+        this.isRightAnswer = this.isRightAnswer.bind(this);
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.isBoolean = this.isBoolean.bind(this);
+        this.isMultiple = this.isMultiple.bind(this);
+        this.displayResults = this.displayResults.bind(this);
     }
-  }
-  isRightAnswer() {
-    this.setState({
-      score: this.state.score + 1
-    });
-    this.getQuestion();
-  }
-  handleOnClick = (event) => {
-    this.state.cor_answer === event.target.innerText
-    ? this.isRightAnswer()
-    : this.getQuestion();
-  }
 
-  render() {
-    let { current, max, score, answers, question } = this.state;
-    return (
-      <div className="Mquestions">
-        <div>
-          <h2>
-            Questions {current}/{max} Score {score}/{max}
-          </h2>
-          <p>{decodeURIComponent(question)}</p>
-        </div>
-        <div className="buttonBox">
-          <ul>
-            <button
-              id="button1"
-              onClick={this.handleOnClick}
-            >
-              {decodeURIComponent(answers[0])}
-            </button>
-            <button
-              id="button2"
-              onClick={this.handleOnClick}
-            >
-              {decodeURIComponent(answers[1])}
-            </button>
-            <button
-              id="button3"
-              onClick={this.handleOnClick}
-            >
-              {decodeURIComponent(answers[2])}
-            </button>
-            <button
-              id="button4"
-              onClick={this.handleOnClick}
-            >
-              {decodeURIComponent(answers[3])}
-            </button>
-          </ul>
-        </div>
-      </div>
-    );
-  }
+    componentDidMount() {
+        axios
+            .get(datafromjson.url)
+            .then(response => response.data)
+            .then(data => {
+                this.setState({
+                    apidata: data
+                });
+                this.getQuestion();
+
+
+            });
+    }
+
+    getQuestion() {
+        let { current, apidata } = this.state;
+        if (apidata.results[current].type === "multiple") {
+            this.isMultiple()
+        } else {
+            this.isBoolean()
+        }
+    }
+
+    isMultiple() {
+        let { current, apidata } = this.state;
+        let incorrect_answer = apidata.results[current].incorrect_answers
+        let correctAnswer = apidata.results[current].correct_answer
+        this.setState({
+            question: apidata.results[current].question,
+            answers: [
+                [incorrect_answer[0]],
+                [incorrect_answer[1]],
+                [incorrect_answer[2]],
+                [correctAnswer]
+            ].sort(() => Math.random() - 0.5),
+            cor_answer: correctAnswer,
+            max: apidata.results.length,
+            current: current + 1
+        });
+    }
+
+    isBoolean() {
+        let { current, apidata } = this.state;
+        this.setState({
+            question: apidata.results[current].question,
+            answers: [["True"], ["False"]],
+            cor_answer: apidata.results[current].correct_answer,
+            max: apidata.results.length,
+            current: current + 1,
+
+        });
+    }
+
+    isRightAnswer() {
+        this.setState({
+            score: this.state.score + 1
+        }
+        );
+    }
+
+    displayResults() {
+        let { score, max, cor_answer } = this.state;
+        data.score = score;
+        data.max = max;
+        data.cor_answer = cor_answer;
+        console.log(data.score, this.state.current, "current");
+        history.push("/Zsgameresult");
+    }
+    /*fixed the results display to update the score before displaying the results, if someone can come up with something cleaner feel free to modify*/
+    handleOnClick = (event) => {
+        let { apidata, current } = this.state
+        if (this.state.cor_answer === event.target.innerText && apidata.results.length !== current) {
+            this.isRightAnswer();
+            this.getQuestion();
+        }
+        else if (this.state.cor_answer !== event.target.innerText && apidata.results.length !== current) {
+            this.getQuestion();
+        }
+        else if (this.state.cor_answer === event.target.innerText && apidata.results.length === current) {
+            this.setState({ score: this.state.score + 1 },
+                () => this.displayResults()
+            );
+        }
+        else if (this.state.cor_answer !== event.target.innerText && apidata.results.length === current) {
+            this.displayResults()
+        }
+    }
+
+    render() {
+        let { current, max, score, question } = this.state;
+        return (
+            <div className="Mquestions">
+                <div>
+                    <h2>
+                        Questions {current}/{max} Score {score}/{max}
+                    </h2>
+                    <p>{decodeURIComponent(question)}</p>
+                </div>
+                <div className="buttonBox">
+                { /*made the buttons display dynamic*/ 
+                    this.state.answers.map(element => {
+                    return <div><button onClick={this.handleOnClick}> 
+                        {decodeURIComponent(element)}                
+                        </button></div>
+                    })
+                }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Mquestions;
