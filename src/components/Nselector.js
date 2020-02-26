87644dde53779cf307655bb3f6b02827b78a7ac5
KrNikk <krisko.nikolett@gmail.com>
2020-02-26 13:26:08 +0100

    Add css

--- src/components/Nselector.js
+++ src/components/Nselector.js
@@ -1,6 +1,7 @@
 import React from "react";
 import history from "./../history";
 import data from "./data";
+import Nselector from "./Nselector.css";
 
 const datafromjson = data;
 
@@ -50,55 +51,89 @@ class Selector extends React.Component {
 
   render() {
     return (
-      <form>
-        <p>Choose your game:</p>
-        <input
-          type="number"
-          min="1"
-          max="50"
-          onChange={this.handleQuestionNum}
-          value={this.state.number}
-        ></input>
-        <select value={this.state.value} onChange={this.handleChange}>
-          <option value="">Any Category</option>
-          <option value="9">General Knowledge</option>
-          <option value="10">Entertainment: Books</option>
-          <option value="11">Entertainment: Film</option>
-          <option value="12">Entertainment: Music</option>
-          <option value="13">Entertainment: Musicals &amp; Theatres</option>
-          <option value="14">Entertainment: Television</option>
-          <option value="15">Entertainment: Video Games</option>
-          <option value="16">Entertainment: Board Games</option>
-          <option value="17">Science &amp; Nature</option>
-          <option value="18">Science: Computers</option>
-          <option value="19">Science: Mathematics</option>
-          <option value="20">Mythology</option>
-          <option value="21">Sports</option>
-          <option value="22">Geography</option>
-          <option value="23">History</option>
-          <option value="24">Politics</option>
-          <option value="25">Art</option>
-          <option value="26">Celebrities</option>
-          <option value="27">Animals</option>
-          <option value="28">Vehicles</option>
-          <option value="29">Entertainment: Comics</option>
-          <option value="30">Science: Gadgets</option>
-          <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
-          <option value="32">Entertainment: Cartoon &amp; Animations</option>
-        </select>
-        <select value={this.state.difficulty} onChange={this.handleDiff}>
-          <option value="">Any Difficulty</option>
-          <option value="easy">Easy</option>
-          <option value="medium">Medium</option>
-          <option value="hard">Hard</option>
-        </select>
-        <select value={this.state.type} onChange={this.handleType}>
-          <option value="any">Any Type</option>
-          <option value="multiple">Multiple</option>
-          <option value="boolean">True / False</option>
-        </select>
-        <input type="submit" value="Let's play!" onClick={this.handleSubmit} />
-      </form>
+      <div id="form">
+        <p id="chooseName">CHOOSE YOUR GAME:</p>
+        <div>
+          <label>Category: </label>
+          <select
+            className="params"
+            value={this.state.value}
+            onChange={this.handleChange}
+          >
+            <option value="">Any Category</option>
+            <option value="9">General Knowledge</option>
+            <option value="10">Entertainment: Books</option>
+            <option value="11">Entertainment: Film</option>
+            <option value="12">Entertainment: Music</option>
+            <option value="13">Entertainment: Musicals &amp; Theatres</option>
+            <option value="14">Entertainment: Television</option>
+            <option value="15">Entertainment: Video Games</option>
+            <option value="16">Entertainment: Board Games</option>
+            <option value="17">Science &amp; Nature</option>
+            <option value="18">Science: Computers</option>
+            <option value="19">Science: Mathematics</option>
+            <option value="20">Mythology</option>
+            <option value="21">Sports</option>
+            <option value="22">Geography</option>
+            <option value="23">History</option>
+            <option value="24">Politics</option>
+            <option value="25">Art</option>
+            <option value="26">Celebrities</option>
+            <option value="27">Animals</option>
+            <option value="28">Vehicles</option>
+            <option value="29">Entertainment: Comics</option>
+            <option value="30">Science: Gadgets</option>
+            <option value="31">
+              Entertainment: Japanese Anime &amp; Manga
+            </option>
+            <option value="32">Entertainment: Cartoon &amp; Animations</option>
+          </select>
+        </div>
+        <div className="params">
+          <label className="params">Number of questions: </label>
+          <input
+            className="params"
+            type="number"
+            min="1"
+            max="50"
+            onChange={this.handleQuestionNum}
+            value={this.state.number}
+          ></input>
+        </div>
+        <div className="params">
+          <label>Difficulty: </label>
+          <select
+            className="params"
+            value={this.state.difficulty}
+            onChange={this.handleDiff}
+          >
+            <option value="">Any Difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </div>
+        <div className="params">
+          <label>Type: </label>
+          <select
+            className="params"
+            value={this.state.type}
+            onChange={this.handleType}
+          >
+            <option value="any">Any Type</option>
+            <option value="multiple">Multiple</option>
+            <option value="boolean">True / False</option>
+          </select>
+        </div>
+        <button
+          className="button "
+          type="submit"
+          value="Let's play!"
+          onClick={this.handleSubmit}
+        >
+          Let's play!
+        </button>
+      </div>
     );
   }
 }
